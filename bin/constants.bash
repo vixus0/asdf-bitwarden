d869c04a66c7fd91a6b47767c70089f81bd7011f
michaeldelago <michaeldelago98@gmail.com>
2023-03-04 16:54:47 -0500

    work with new repo

---
+++ bin/constants.bash
@@ -0,0 +1,9 @@
+# required environment variables
+: "${ASDF_INSTALL_VERSION?}"
+: "${ASDF_INSTALL_PATH?}"
+: "${ASDF_DOWNLOAD_PATH?}"
+
+toolname="bw"
+old_repository="bitwarden/cli"
+repository="bitwarden/clients"
+prefix="cli-"
